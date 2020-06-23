8f15e40fecea7b17f9ac8c9681fd8886bf206b66
Daniel Gould <dmgould310@gmail.com>
2020-06-23 14:40:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const getFirstSelector = selector => {
+  return document.querySelector;
+};
+const nestedTarget = () => {
+return document.getElementById('nested').querySelector('div.target')
+  };
+  
+const increaseRankBy = n => {
+  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
+  for (let i = 0; i < upRank.length; i++){
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
+    }
+};
 
+function deepestChild(){
+  var deep = document.getElementById('grand-node').querySelectorAll('div')
+  return deep[deep.length-1]
+
+}
